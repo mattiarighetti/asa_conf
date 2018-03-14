63a3444b7044af2f5376b248c7e323f32481a04f
Mattia Righetti <studio@mattiarighetti.it>
2018-03-14 16:42:34 +0100

    Aggiunta files.

--- site/wwwroot/clienti-canc.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include 'db-config.php';
-	$conn->query("DELETE FROM ac_clienti ")if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
-} elsheader("location: /nuova-pagina.php");
-: " . $conn->error;
-}
