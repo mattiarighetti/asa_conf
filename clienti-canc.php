8c4cf0dde75f8c7e200afab9c02bca8f9ed2d54f
Mattia Righetti <studio@mattiarighetti.it>
2018-03-21 18:12:04 +0100

    Sistemazione funzionalità cancellazione cliente.

--- clienti-canc.php
+++ clienti-canc.php
@@ -1,13 +1,9 @@
 <?php
-
-	include 'db-config.php';
-
-	$conn->query("DELETE FROM ac_clienti ")if ($conn->query($sql) === TRUE) {
-
-    echo "Record deleted successfully";
-
-} elsheader("location: /nuova-pagina.php");
-
-: " . $conn->error;
-
+include('db-config.php');
+if(isset($_GET[ 'cliente_id'])) {
+	$prenotazione_id = $_GET['cliente_id'];
+	$result = mysqli_query($connection, "DELETE FROM ac_clienti WHERE cliente_id=$cliente_id") or die(mysqli_error($connection));
+	header("Location: clienti-list.php");
+} else {
+	header("Location: clienti-list.php");
 }
