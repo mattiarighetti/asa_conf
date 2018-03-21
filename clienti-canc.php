1f7031953f0b2256a705eb8cb901ad9f58b3f675
Mattia Righetti <studio@mattiarighetti.it>
2018-03-21 18:18:28 +0100

    Correzione errore cancellazione.

--- clienti-canc.php
+++ clienti-canc.php
@@ -1,7 +1,7 @@
 <?php
 include('db-config.php');
 if(isset($_GET[ 'cliente_id'])) {
-	$prenotazione_id = $_GET['cliente_id'];
+	$cliente_id = $_GET['cliente_id'];
 	$result = mysqli_query($connection, "DELETE FROM ac_clienti WHERE cliente_id=$cliente_id") or die(mysqli_error($connection));
 	header("Location: clienti-list.php");
 } else {
